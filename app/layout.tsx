cad5e2951404524200dcc7be0e73eee28a156e41
Abul Qasim <abulqasim@Abuls-MacBook-Air.local>
2026-02-09 12:10:46 -0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,6 +72,19 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" className="scroll-smooth">
+            <head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-QPYLK2PRJ5"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-QPYLK2PRJ5');
+                        `,
+                    }}
+                />
+            </head>
             <body className={`${inter.variable} ${poppins.variable} ${jetbrainsMono.variable} font-sans`}>
                 <Navigation />
                 {children}
